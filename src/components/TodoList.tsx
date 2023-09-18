640e78b23f5186c530cb1b788523d0d1827e1ee9
Michael Houmann <mhoumann87@gmail.com>
2023-09-18 13:29:12 +0200

    Add boilerplate files, start of addinput component and add dependencies

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,7 @@
+import { TodoItem } from './TodoItem';
+import { useTodo } from '.';
+import { SiStarship } from 'react-icons/si';
+
+export const TodoList = () => {
+  return <div>To Do List</div>;
+};
